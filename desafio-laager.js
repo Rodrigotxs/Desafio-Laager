3295974a8a0d7c105e0c9bbd86bdb28dc78a0746
Rodrigotxs <rodrigo.teixeira_santos@hotmail.com>
2020-10-31 00:29:10 -0300

    terceiro commit

--- desafio-laager.js
+++ desafio-laager.js
@@ -14,13 +14,6 @@ class LinkedList {
     }
 
 
-    // Adicionar ao início da lista
-    adcPrimeiro(data) {
-        this.head = new Node(data, this.head);
-
-        this.size++;
-    }
-
     // Adicionar ao final da lista
     adcFinal(data) {
         let node = new Node(data);
@@ -40,36 +33,6 @@ class LinkedList {
         this.size++;
     }
 
-    // Adicionar por posição
-    adcPorIndex(data, index) {
-        // Verificando se a posição existe no intervalo
-        if (index > 0 && index > this.size) {
-            return;
-        }
-
-        // Se for a primeira posição [0]
-        if (index === 0) {
-            this.head = new Node(data, this.head);
-            return;
-        }
-
-        const node = new Node(data);
-        let current, previous;
-
-        current = this.head;
-        let count = 0;
-
-        while (count < index) {
-            previous = current; // Nó anterior
-            count++;
-            current = current.next; // Próximo Nó
-        }
-
-        node.next = current;
-        previous.next = node;
-
-        this.size++; // Adicionando 1 ao tamanho atual
-    }
 
     // Limpar lista
     limparLista() {
@@ -79,56 +42,33 @@ class LinkedList {
 
     //Printar lista
     printLista() {
-            let current = this.head;
-
-            while (current) {
-                console.log(current.data);
-                current = current.next;
-            }
-        }
-        // Invertendo um nó
-    inverteNo(node, prev) {
-            let newNode = new Node(node.data, prev);
-            if (node.next) {
-                return this.inverteNo(node.next, newNode);
-            } else {
-                return newNode;
-            }
-        }
-        // Retorna a linked-list invertida
-    inverteLista() {
-        let current = this.inverteNo(this.head, null);
-
+        let current = this.head;
 
         while (current) {
             console.log(current.data);
             current = current.next;
         }
-        return this.inverteNo(this.head, null);
-
     }
 
+    // Inverte a lista
+    inverteLista() {
+            let aux = null; // Ponteiro auxiliar para inversão de valores
+            let current = this.head;
+            let prox = current.next;
+
+            while (current != null) {
+                current.next = aux;
+                aux = current;
+                current = prox;
+                if (prox != null) {
+                    prox = prox.next;
+                }
+                this.head = aux;
+            }
+        }
+        // Retornar o Head a lista
+    getHead() {
+        return console.log(this.head.data);
+    }
 
 }
-// Tratando data e hora
-function fData(data = new Date(2005, 1, 1, 14, 00, 00)) {
-    var dia = data.getDate();
-    var mes = data.getMonth();
-    var ano = data.getFullYear();
-    var hora = data.getHours();
-    var minutos = data.getMinutes();
-    var segundos = data.getSeconds();
-
-    return dia + '/' + mes + '/' + ano + ' ' + hora + ':' + 0 + minutos + ':' + 0 + segundos
-}
-
-let lista = new LinkedList();
-
-
-lista.adcFinal(100);
-lista.adcFinal(200);
-lista.adcFinal(300);
-lista.adcFinal(400);
-
-lista.printLista();
-lista.inverteLista();
